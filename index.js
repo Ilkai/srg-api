6a02474755f86ec75c059c809257b0af7c4b3253
Lynden Noye <lt.noye@gmail.com>
2018-08-13 18:10:27 +1000

    Added CLI arg parsing

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ var express = require('express');
 var request = require('request');
 var creds = require('./.env.json');
 
-// TODO Take args from CLI for sprints
+console.log('Options:\n \
+  \t--sprint [sprint title]\tthe current running sprint e.g. "MTE Sprint 14" (default, demo)\n \
+  \t--project [key]\t\tthe project key e.g. "MTE" (default).\n \
+  \t--jira [jira domain]\tthe organisation URL for JIRA e.g. "domesticcat.atlassian.net" (default)\n');
+
+// Take args from CLI for sprints
+// e.g. `--sprint 'MTE Sprint 14'`
+var currentSprint;
+var previousSprint;
+var argv = require('minimist')(process.argv.slice(2));
+if (argv.currentSprint) {
+  currentSprint = argv.currentSprint;
+  previousSprint = "Sprint " + (currentSprint.match(/\d+?$/g) - 1);
+} else {
+  currentSprint = "Sprint 14";
+}
+console.log("Prev: " + previousSprint + "\nCurr: " + currentSprint);
+
 // TODO Allow config of Jira instance from default with optional args
 // TODO More endpoints, better naming
 
@@ -11,7 +28,6 @@ var app = express();
 app.get('/lastsprint', function(req, res) {
   var projectKey = 'MTE';
   var previousSprint = 'Sprint 14';
-  var currentSprint = 'Sprint 15';
 
   // Use JQL to fetch completed issues from `previousSprint`
   var baseURL = 'https://domesticcat.atlassian.net/rest/api/2/search';
